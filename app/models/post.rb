cd81f5ba635096271776634745503195d21b59e8
rendnil <andrew.lindner.15@gmail.com>
2018-09-02 21:53:33 -0400

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,21 @@
+require "pry"
 class Post < ActiveRecord::Base
+  validates :title, presence: true
+  validates :content, length: {minimum: 250}
+  validates :summary, length: {maximum: 250}
+  validates :category, inclusion: {in: ["Fiction", "Non-Fiction"]}
+  validate :clickbait?
+
+  def clickbait?
+  
+    if self.title == nil
+      errors.add(:title, "error")
+    elsif ["Won't Believe", "Secret", "Top [number]", "Guess"].none? do |item|
+      (self.title).include?(item)
+    end  == true
+      errors.add(:title, "error")
+    end
+  end
+
+
 end
